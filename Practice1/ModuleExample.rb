b0241f0e94933f5e8fe58b26963caf8954056919
cittemmeghana <meghanareddy.cittem@gmail.com>
2019-04-23 07:14:05 -0500

    module example

--- Practice1/ModuleExample.rb
+++ Practice1/ModuleExample.rb
@@ -1,5 +1,21 @@
 module Password
+  private
+  i = 5
   def getPassword
     return "password@123"
   end
+
+  public
+  def printPassword
+    puts getPassword #this can call getPassword as it is in same module
+  end
 end
+
+class Person
+  include Password
+end
+
+person = Person.new
+puts person.printPassword
+#below statement will fail as getPassword is private
+#puts person.getPassword
