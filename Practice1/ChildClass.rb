bc5aaa6615b90e7124e270ba6a30a50f4d52c006
cittemmeghana <meghanareddy.cittem@gmail.com>
2019-04-22 22:42:33 -0500

    ParentClass and ChildClass practice

--- Practice1/ChildClass.rb
+++ Practice1/ChildClass.rb
@@ -1,1 +1,11 @@
+require_relative 'ParentClass'
+class ChildClass < ParentClass
+def displayMessageFromChild
+  puts "This is from child class"
+end
+end
 
+childClass = ChildClass.new
+
+childClass.displayMessageFromParent
+childClass.displayMessageFromChild
--- Practice1/ParentClass.rb
+++ Practice1/ParentClass.rb
@@ -1,1 +1,8 @@
+class ParentClass
+    def displayMessageFromParent
+      puts "This is from parent class"
+    end
+  end
+
+
 
